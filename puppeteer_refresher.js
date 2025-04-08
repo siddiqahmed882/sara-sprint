0ab747d287a9f057835a3bc022219789567ac3af
ZainulAbidin-dot <zchxvr@gmail.com>
2025-04-08 16:02:58 +0300

    feat: add Puppeteer script for automated login testing

--- puppeteer_refresher.js
+++ puppeteer_refresher.js
@@ -13,34 +13,36 @@ import puppeteer from "puppeteer";
 
         // goto the new page
 
-        await page.goto('https://google.com')
+        await page.goto('http://localhost:5000/Login-OTP/P-Login.html')
 
         // variables
-        const searchSelector = 'input[title="Search"]'
-        const restaurantSelector = '.rllt__details [role="heading"] span'
+        const email = 'input[name="email"]'
+        const password = 'input[name="password"]'
+        // const restaurantSelector = '.rllt__details [role="heading"] span'
 
-        await page.waitForSelector(searchSelector)
-
-        await page.click(searchSelector, { clickCount: 1 })
-
-        await page.type(searchSelector, "Pizza restaurants");
+        await page.waitForSelector(email)
+        await page.click(email, { clickCount: 1 })
+        await page.type(email, "patient01@gmail.com");
+        
+        await page.waitForSelector(password)
+        await page.click(password, { clickCount: 1 })
+        await page.type(password, "Patient@123");
 
         await page.keyboard.press("Enter");
 
-        await page.waitForSelector(restaurantSelector);
+        // await page.waitForSelector(restaurantSelector);
 
         // go ahead and scrape
 
-        const titles = await page.evaluate(
-            (rs) => {
-                const restaurantTitlesSpanTag = document.querySelectorAll(rs) //DOM
-                const restaurantTitles = [...restaurantTitlesSpanTag].map(el => el.textContent)
-                return restaurantTitles
-            }, restaurantSelector)
+        // const titles = await page.evaluate(
+        //     (rs) => {
+        //         const restaurantTitlesSpanTag = document.querySelectorAll(rs) //DOM
+        //         const restaurantTitles = [...restaurantTitlesSpanTag].map(el => el.textContent)
+        //         return restaurantTitles
+        //     }, restaurantSelector)
 
 
 
-        console.log(titles)
         console.log("done with automation")
 
     }
